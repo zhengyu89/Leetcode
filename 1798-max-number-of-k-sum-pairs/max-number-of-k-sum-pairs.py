cd6fe9de7b7d69a9cdff21e1a662222edd2655af
Tan Zheng Yu <148403225+zhengyu89@users.noreply.github.com>
2025-04-24 22:30:46 +0800

    Time: 520 ms (26.64%) | Memory: 30.6 MB (35.41%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,25 +1,16 @@
 class Solution:
-    def maxOperations(self, nums: List[int], k: int) -> int:
-        # making hash map
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] = 1
-        
-        maxOperations = 0
-    
-        for num in list(freq.keys()):
-            complement = k - num
-            if complement in freq:
-                if num == complement:
-                    pairs = freq[num] // 2
-                else:
-                    pairs = min(freq[num], freq[complement])
-                
-                maxOperations += pairs
-                freq[num] -= pairs
-                freq[complement] -= pairs
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        map = dict()
+        count = 0
 
-        return maxOperations
+        for i in nums:
+            x = k - i
+            if x in map and map[x] > 0: # if compliment,x is found in map 
+                                        # and there is not zero key of x
+                count += 1              # take a count as pair
+                map[x] -= 1             # delete the x used, i is moved to the next
+                                        # element
+            else:
+                map[i] = map.get(i, 0) + 1  # add into dictionary if not paired
+        
+        return count
