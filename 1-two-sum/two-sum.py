6b7a38211d7230a0a0c953e5846894ed97c1b915
Tan Zheng Yu <148403225+zhengyu89@users.noreply.github.com>
2025-04-26 12:47:34 +0800

    Time: 5 ms (36.81%) | Memory: 19 MB (34.42%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i in range(len(nums)):
+            h[nums[i]] = i  #return latest occurance
+        
+        for i in range(len(nums)):
+            y = target - nums[i] # find complement
+            if y in h and i != h[y] :
+                return [i , h[y]]
+
+# Time complexity = O(n)
+# Space complexity = o(n)
